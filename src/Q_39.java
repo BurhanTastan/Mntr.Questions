fb0065c3d774d95da63017899dbe38773fb25476
Burhan Tastan <119176022+BurhanTastan@users.noreply.github.com>
2022-12-24 22:50:23 +0100

    Q_39

---
+++ src/Q_39.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Q_39 {
+    public static void main(String[] args) {
+
+        /*Q-39)
+        Bir tamsayı dizisini (Int Array) girdi olarak kabul eden
+        ve verilen bu (Arrayden) diziden minimum ve maksimum sayıları yazdıran bir yöntem yazın
+        */
+
+        int[] nummer = {-1,-2,-3,4,7,9,123,5};
+
+        MaxundMinNummer(nummer);
+
+
+    }
+
+    private static void MaxundMinNummer(int[] nummer) {
+
+        Arrays.sort(nummer);
+
+        System.out.println("Min.nummer"+nummer[0]+"\n"+"Max.nummer"+nummer[nummer.length-1]);
+
+
+    }
+
+
+}
