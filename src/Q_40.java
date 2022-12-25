c782d4eb78f6d89620d8b2f639be334f913a805c
Burhan Tastan <119176022+BurhanTastan@users.noreply.github.com>
2022-12-25 15:54:09 +0100

    Q_40

---
+++ src/Q_40.java
@@ -0,0 +1,25 @@
+public class Q_40 {
+    public static void main(String[] args) {
+
+        /*Q-40)
+        Bir diziyi (Array) parametre olarak kabul eden ve dizideki tüm elemanların toplamını döndüren
+         bir method yazın ve ardından sonucu main methodda yazdırın.
+        */
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        System.out.println(sayilarinToplami(arr));
+
+
+    }
+
+    private static int sayilarinToplami(int[] arr) {
+        int toplam = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            toplam += arr[i];
+
+        }
+        return toplam;
+    }
+}
