c67649b0516cde66707484646c2cf096ed0d554b
Burhan Tastan <119176022+BurhanTastan@users.noreply.github.com>
2022-12-25 16:56:39 +0100

    Q_41

---
+++ src/Q_41.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+
+public class Q_41 {
+    public static void main(String[] args) {
+
+        /*Parametre olarak 2 Int Array kabul eden ve 2 Array listesini
+        yeni bir Arraye ekleyen ve yazdıran bir method yazın.
+                * Input1={1,2,3,4}
+                * Input2={5,6}
+                * Çıktı={1,2,3,4,5,6}
+
+         */
+
+        int[] arr1 = {1,2,3,4};
+        int[] arr2 = {5,6};
+
+        method(arr1,arr2);
+
+    }
+
+    private static void method(int[] arr1, int[] arr2) {
+
+        int[] birlestirme = new int[arr1.length+ arr2.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            birlestirme[i]=arr1[i];
+        }
+        for (int j = 0; j < arr2.length; j++) {
+            birlestirme[arr1.length+j]=arr2[j];
+        }
+        System.out.println(Arrays.toString(birlestirme));
+
+    }
+}
