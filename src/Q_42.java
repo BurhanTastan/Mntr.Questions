080bedfd6cb5b98165391ce2a32ffbc1944c40eb
Burhan Tastan <119176022+BurhanTastan@users.noreply.github.com>
2022-12-25 18:51:14 +0100

    Q_42

---
+++ src/Q_42.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Q_42 {
+    public static void main(String[] args) {
+
+        /*Q_42-)
+        Parametre olarak birden fazla tamsayı (integer) kabul eden
+        ve bu integer sayıların toplamını yazdıran bir dönüş methodu (void) yazın
+        Method name=sum
+        eğer metodu bu şekilde çağırırsanız
+
+        toplam(1,2,3) çıktı =6
+        toplam(1,2,3,4,5) çıktı =15
+        sum(1,2) output=3
+        Ipucu:  varargs sorusu, varargs olusturalım
+
+         */
+
+        toplam(1,2,3);
+
+    }
+
+    private static void toplam(int... sayilar) {
+        int toplam=0;
+        for (int i = 0; i < sayilar.length ; i++) {
+            toplam+= sayilar[i];
+        }
+        System.out.println(toplam);
+    }
+}
